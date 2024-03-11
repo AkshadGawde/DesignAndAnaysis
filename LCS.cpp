94c4d080971f060ef1fd8f4a8ca2b236622fa98b
akshad Gawde <gawdeakshad@gmail.com>
2024-03-11 20:48:57 +0530

    Exp-7

---
+++ LCS.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string longestCommonSubsequence(const string &X, const string &Y)
+{
+    int m = X.length();
+    int n = Y.length();
+
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+    for (int i = 1; i <= m; ++i)
+    {
+        for (int j = 1; j <= n; ++j)
+        {
+            if (X[i - 1] == Y[j - 1])
+            {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            }
+            else
+            {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    string lcs;
+    int i = m, j = n;
+    while (i > 0 && j > 0)
+    {
+        if (X[i - 1] == Y[j - 1])
+        {
+            lcs = X[i - 1] + lcs;
+            --i;
+            --j;
+        }
+        else if (dp[i - 1][j] > dp[i][j - 1])
+        {
+            --i;
+        }
+        else
+        {
+            --j;
+        }
+    }
+    return lcs;
+}
+int main()
+{
+    string X1 = "EBTBCADF";
+    string Y1 = "ABBCDGF";
+    string X2 = "ABCBDABA";
+    string Y2 = "BDCABAA";
+
+    cout << "LCS for X1 and Y1: " << longestCommonSubsequence(X1, Y1) << endl;
+    cout << "LCS for X2 and Y2: " << longestCommonSubsequence(X2, Y2) << endl;
+
+    return 0;
+}
